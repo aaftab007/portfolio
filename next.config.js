0f50fd921985e3e773802b697151aa4316c617dd
Aaftab Mohammad <aaftab786mohd@gmail.com>
2025-10-12 23:08:37 -0400

    Fix next.config.js: remove invalid fontLoaders property

--- next.config.js
+++ next.config.js
@@ -7,15 +7,6 @@ const nextConfig = {
   images: {
     domains: ["user-images.githubusercontent.com", "cdn.hashnode.com", "github.com"],
   },
-  fontLoaders: [
-    {
-      loader: "@next/font/google",
-      options: {
-        subsets: ["latin"],
-        weight: ["400", "500", "600", "700", "800"],
-      },
-    },
-  ],
 };
 
 module.exports = nextConfig;
